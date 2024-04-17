79219926434bab514dc5a10f85a5e5e4738efada
ThanhSon-3112 <son54029@gmail.com>
2024-04-17 21:30:28 +0700

    start Menu

--- The Knight Return/Assets/_Script/Menu/StartGameMenu.cs
+++ The Knight Return/Assets/_Script/Menu/StartGameMenu.cs
@@ -1,4 +1,4 @@
-/*using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -33,4 +33,3 @@ public class StartGameMenu : MonoBehaviour
         Application.Quit();
     }
 }
-*/
