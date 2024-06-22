b91042754d1fb3b295dcde554237dedd9673669c
ThanhSon-3112 <son54029@gmail.com>
2024-06-22 20:27:36 +0700

    Remove LFS tracking

--- The Knight Return/Assets/_Script/Interface/IDamageableEnemy.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IDamageableEnemy
-{
-    void TakePlayerDamage(float _damageDone, Vector2 _hitDirection, float _hitForce);
-}
